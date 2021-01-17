777630b35792669ea850619cc1b467c82c05a74d
文泓远 <2534723586@qq.com>
2021-01-17 12:06:51 +0800

    今日头条移动端第一次提交

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
+
+// 生产环境提示关闭
 Vue.config.productionTip = false
 
+// 创建 Vue 根组件 APP，将路由， vuex 挂载上去
+// 把 APP 根组件 渲染到 #app 节点
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
